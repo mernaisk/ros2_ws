035ce4638dd4bbffb880d200f5caf0de5f504591
mernaisk <mernaiskander35@gmail.com>
2025-04-30 17:39:40 +0200

    test

--- build/cmd_vel_to_joystick/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/pi2/ros2_ws/install/my_parameters/share/my_parameters/package.sh"
--- build/csm/ament_cmake_symlink_install_targets_0_Release.cmake
+++
@@ -1,1 +0,0 @@
-ament_cmake_symlink_install_targets("TARGET_FILES" "/home/pi2/ros2_ws/build/csm/libcsm.so" "TARGETS" "csm" "ARCHIVE_DESTINATION" "lib" "LIBRARY_DESTINATION" "lib" "RUNTIME_DESTINATION" "lib")
